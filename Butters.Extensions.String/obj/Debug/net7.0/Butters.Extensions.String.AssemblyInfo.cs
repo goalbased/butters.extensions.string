b3a6a4158539fb450c902824a85b1d3e8f244297
michael <knicksmojs@gmail.com>
2023-01-17 17:01:22 +0800

    auto push

--- Butters.Extensions.String/obj/Debug/net7.0/Butters.Extensions.String.AssemblyInfo.cs
+++ Butters.Extensions.String/obj/Debug/net7.0/Butters.Extensions.String.AssemblyInfo.cs
@@ -14,12 +14,12 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyCopyrightAttribute("2023 Butters")]
 [assembly: System.Reflection.AssemblyDescriptionAttribute("A high performance string extension")]
-[assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.3.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.3")]
+[assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.4.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.4")]
 [assembly: System.Reflection.AssemblyProductAttribute("Butters.Extensions.String")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Butters.Extensions.String")]
-[assembly: System.Reflection.AssemblyVersionAttribute("1.0.3.0")]
-[assembly: System.Reflection.AssemblyMetadataAttribute("RepositoryUrl", "https://github.com/goalbased/butters.extensions.string")]
+[assembly: System.Reflection.AssemblyVersionAttribute("1.0.4.0")]
+[assembly: System.Reflection.AssemblyMetadataAttribute("RepositoryUrl", "https://github.com/goalbased/butters.extensions.string/")]
 
 // 由 MSBuild WriteCodeFragment 類別產生。
 
